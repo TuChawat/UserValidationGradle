b09f7d8317d89e03073713f4127ecdb12357139c
TuChawat <76853681+TuChawat@users.noreply.github.com>
2021-04-07 08:03:46 +0530

    Update UserRegistrationTest.java

--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -32,5 +32,79 @@ public class UserRegistrationTest {
         boolean result = userValidationProcess.validateFirstName("tu");
         Assertions.assertFalse(result);
     }
+    @Test
+    public void givenLastName_WhenLastNameStartsWithCapitalLetter_ShouldReturn_True() {
 
+        boolean result = userValidationProcess.validateLastName("Chawat");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void givenLastName_WhenLastNameInLowerCase_ShouldReturn_false() {
+
+        boolean result = userValidationProcess.validateLastName("chawat");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void givenLastName_WhenLastNameStartHaveMinThreeLetters_ShouldReturn_True() {
+
+        boolean result = userValidationProcess.validateLastName("Cha");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void givenLastName_WhenLastNameStartWithLessThanThreeLetters_ShouldReturn_False() {
+
+        boolean result = userValidationProcess.validateLastName("ch");
+        Assertions.assertFalse(result);
+    }
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnTrue() {
+
+        boolean result = userValidationProcess.validEmail("abc.xyz@bl.co.in");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenMobNumber_WhenProper_ShouldReturnTrue() {
+
+        boolean result = userValidationProcess.validMobileNo("91 9689887638");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenMobNumber_WhenDifferent_ShouldReturnFalse() {
+
+        boolean result = userValidationProcess.validMobileNo("91 968988B638");
+        Assertions.assertEquals(false, result);
+
+    }
+
+    @Test
+    public void givenPasswordRule1_WhenProper_ShouldReturnTrue() {
+
+        boolean result = userValidationProcess.validPassword("jdhuihjn");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPasswordRule2_WhenProper_ShouldReturnTrue() {
+
+        boolean result = userValidationProcess.validPasswordAtleastOneUpperCase("jdFuifjn");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPasswordRule3_WhenProper_ShouldReturnTrue() {
+
+        boolean result = userValidationProcess.validateAlteastOneNumber("jdF5iOj8");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPasswordRule4_WhenProper_ShouldReturnFalse() {
+
+        boolean result = userValidationProcess.validSpecialChar("Eyu6@uyh");
+        Assertions.assertEquals(false, result);
+    }
 }
